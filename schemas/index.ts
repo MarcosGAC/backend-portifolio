3b5f499c98adcb845c85d5d7176958b6fe946dd4
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-06-06 18:44:25 -0300

    creating skills schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import header from "./header";
 import about from "./about";
+import skills from "./skills";
 
-export const schemaTypes = [header,about]
+export const schemaTypes = [header,about,skills]
 
